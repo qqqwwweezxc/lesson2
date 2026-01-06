d812253086290636ef3f7dfaa65bd99d99cd9de7
qqqwwweezxc <vitos.burdash@gmail.com>
2026-01-06 23:14:32 +0000

    lesson 4

---
+++ lessons_hillel/lesson_4_1.py
@@ -0,0 +1,18 @@
+# Написати програму, яка переміщає всі нулі у кінець списку.
+# Ваше завдання — змінити список так, щоб усі нулі опинилися наприкінці списку.
+# Порядок ненульових чисел має зберегтися!
+
+# numbers = [1, 0, 6, 7, 0, 12, 43, 21]
+# numbers_sorted = []
+# count = 0
+
+# for i in numbers:
+#     if i != 0:
+#         numbers_sorted.append(i)
+#     else:
+#         count += 1
+
+# for _ in range(count):
+#     numbers_sorted.append(0)
+
+# print(numbers_sorted)
---
+++ lessons_hillel/lesson_4_3.py
@@ -0,0 +1,9 @@
+# 1. Необхідно створіти список випадкових чисел із випадковою кількістю елементів від 3 до 10.
+# 2. Створить інший список з 3 елементів зі списку з п.1 - першим, третім і другим з кінця.
+
+# import random
+# num = random.randint(3, 10)
+# numbers = [random.randint(1, 9) for _ in range(num)]
+# numbers_sorted = [numbers[0], numbers[2], numbers[-2]]
+# print(numbers)
+# print(numbers_sorted)
